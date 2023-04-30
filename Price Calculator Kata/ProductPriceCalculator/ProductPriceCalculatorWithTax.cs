ca3fddfd698f90f66ac0595defe0caac9a186fa4
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-04-30 15:45:16 +0300

    Precision: Edit calculating precision

--- Price Calculator Kata/ProductPriceCalculator/ProductPriceCalculatorWithTax.cs
+++ Price Calculator Kata/ProductPriceCalculator/ProductPriceCalculatorWithTax.cs
@@ -11,13 +11,15 @@ namespace Price_Calculator_Kata.ProductPriceCalculator
     {
         public IProduct product { get; set; }
         public string currency { get; set; }
+        public int precision { get; set; }
         public ITax tax { get; set; }
 
-        public ProductPriceCalculatorWithTax(IProduct product, string currency, ITax tax)
+        public ProductPriceCalculatorWithTax(IProduct product, string currency, ITax tax, int precision)
         {
             this.product = product;
             this.currency = currency;
             this.tax = tax;
+            this.precision = precision;
         }
 
         public float CalculatePrice()
