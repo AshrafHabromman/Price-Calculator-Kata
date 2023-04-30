89ca0fd48796ea877238cac3ffc386dd1edcdc82
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-04-30 16:55:51 +0300

    Precision: Implement precision test case

--- Price Calculator Kata/Program.cs
+++ Price Calculator Kata/Program.cs
@@ -11,44 +11,35 @@ using Price_Calculator_Kata.Tax;
 
 Console.Write("Please enter the tax percentage you would like: ");
 double taxPercentage = Convert.ToDouble(Console.ReadLine());
-//Console.Write("Please enter the discount percentage you would like: ");
-//double discountPercentage = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please enter the discount percentage you would like: ");
+double discountPercentage = Convert.ToDouble(Console.ReadLine());
 
 ITax taxCalculator = new TaxCalculator((float)taxPercentage);
 
-//float upcDiscount = 0.07f;
-//IDiscount discount = new Discount((float)discountPercentage, false);
-//IDiscount specialUPCDiscount = new SpecialUPCDiscount(1234, (float)upcDiscount, false);
+float upcDiscount = 0.07f;
+IDiscount discount = new Discount((float)discountPercentage, false);
+IDiscount specialUPCDiscount = new SpecialUPCDiscount(1234, (float)upcDiscount, false);
 
 IProduct product = new Product("Potato", 1234, 20.25f);
-IProduct product1 = new Product("Potato", 1234, 17.76f);
-//List<IDiscount> discounts = new List<IDiscount>
-//{
-//    discount,
-//    specialUPCDiscount
-//};
 
-//IExpense packaging = new PercentageExpense("Packaging", 0.01f);
-//IExpense transport = new AmountExpense("Transport", 2.2f);
-//List<IExpense> expenses = new List<IExpense>
-//{
-//    packaging,
-//    transport
-//};
+List<IDiscount> discounts = new List<IDiscount>
+{
+    discount,
+    specialUPCDiscount
+};
+
+IExpense transport = new PercentageExpense("Transport", 0.03f);
+List<IExpense> expenses = new List<IExpense>
+{
+    transport
+};
 
 
 IProductPrinter productPrinter = new
-    ProductWithTaxPrinter(product, "USD",
-    taxCalculator);
+    ProductWithTaxDiscountsAndExpensesPrinter(product, "USD",
+    taxCalculator, discounts, new AmountCap(10f)
+    , expenses, 4, 2);
 
 productPrinter.PrintPrice();
-
-Console.WriteLine();
-
-IProductPrinter productPrinter1 = new
-    ProductWithTaxPrinter(product1, "GBP",
-    taxCalculator);
-
-productPrinter1.PrintPrice();
 
 Console.WriteLine();
